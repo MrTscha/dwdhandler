ae528f87bc5e38018e11880df0c71b14630ddf87
MrTscha <tobias.schad@googlemail.com>
2023-08-17 19:06:48 +0200

    Update __init__.py to load module return_period

--- src/dwdhandler/__init__.py
+++ src/dwdhandler/__init__.py
@@ -1,5 +1,6 @@
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .dow_handler import dow_handler
 from .plotting import plotconstr
 from .calculation import calc_stats
+from .calculation import return_period
