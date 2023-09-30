8a2882e21f6dc222a32187be8f4897678ca00be1
MrTscha <tobias.schad@googlemail.com>
2023-09-30 17:56:30 +0200

    Adding regional averages to deal in sqlite database

--- src/dwdhandler/constants/filedata.py
+++ src/dwdhandler/constants/filedata.py
@@ -14,6 +14,7 @@ RASTER_FOLDER   = 'raster_data/'
 REGAVG_FOLDER   = 'regavg/'
 NWP_FOLDER      = 'nwp_data/'
 SQLITEFILESTAT  = 'DWD_STATION.sqlite'
+SQLITEREGAVG    = 'DWD_regavg.sqlite'
 
 # Available dtypes
 DTYPEAVAIL = ['station','raster','regavg','nwp']
